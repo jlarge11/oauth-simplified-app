ff105a8ffb8fcf36ac69c4a24f19221c71c9ca1b
Justin Large <justin.large@keap.com>
2024-07-31 13:39:58 -0700

    CORS update to allow the client app as an origin

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
